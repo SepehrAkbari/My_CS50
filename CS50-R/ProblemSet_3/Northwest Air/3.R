258e02665a0195a1ef292286e7558df3b485e7e2
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-10-31 13:24:56 -0500

    Delete CS50-R directory

--- CS50-R/ProblemSet_3/Northwest Air/3.R
+++
@@ -1,6 +0,0 @@
-load("air.RData")
-
-air <- air |>
-  filter(county == "OR - Clackamas")
-
-save(air, file = "3.RData")
--- CS50-R/ProblemSet_3/Northwest Air/5.R
+++
@@ -1,8 +0,0 @@
-load("air.RData")
-
-air <- air |>
-  group_by(county) |>
-  slice_max(order_by = emissions) |>
-  ungroup()
-
-save(air, file = "5.RData")
